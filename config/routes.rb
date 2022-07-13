fa2bb6243564ecaab90a1a8744be90edd617394e
oleg <o1eg.34ac@gmail.com>
2022-07-13 17:15:19 +0300

    Remove useless code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'market/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
