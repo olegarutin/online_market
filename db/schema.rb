83af9f1eb1179e7cb9095f05a342e85caf75e093
oleg <o1eg.34ac@gmail.com>
2022-07-13 19:54:32 +0300

    Correct mistake

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_151439) do
     t.string "title"
     t.text "description"
     t.string "phone"
-    t.string "adress"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
