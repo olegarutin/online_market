70270dde570141278596a7dba92989357c6016b8
oleg <o1eg.34ac@gmail.com>
2022-07-15 14:57:27 +0300

    Create index action and before filter for show action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,13 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: :show
+
+  def index
+    @categories = Category.all
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 end
