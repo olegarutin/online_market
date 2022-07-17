bcfd2702bcc9154672537c11684abc57a66bca58
oleg <o1eg.34ac@gmail.com>
2022-07-17 22:31:06 +0300

    Create current_cart helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_cart
+
+  def current_cart
+    Cart.find_or_create_by(user: current_user, status: 'in_progress')
+  end
 end
