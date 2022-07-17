728ccb8f919d8c83ae33d3d1fce4ce39dfb8256a
oleg <o1eg.34ac@gmail.com>
2022-07-17 22:28:37 +0300

    Use enum for status column

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,12 @@
 class Cart < ApplicationRecord
+  IN_PROGRESS = :in_progress
+  ORDERED = :ordered
+
+  STATUSES = [IN_PROGRESS, ORDERED].freeze
+
   belongs_to :user
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
+
+  enum status: STATUSES
 end
