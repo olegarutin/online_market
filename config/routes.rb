005b0dbc28897b6a1544d809eecda6d48719f788
oleg <o1eg.34ac@gmail.com>
2022-07-18 21:28:57 +0300

    Create orders controler and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :carts, only: :show
+  resources :orders
   resource :line_items, except: :show
   resources :markets, :categories, only: %i[index show]
   resources :products, only: :index
