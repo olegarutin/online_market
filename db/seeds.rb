349ae6967c2c16a4eb6ca6052d74cd5c5845e7ae
oleg <o1eg.34ac@gmail.com>
2022-07-21 21:40:01 +0300

    Add image for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 10.times do
   Market.create(title: Faker::Company.name, description: Faker::Company.industry, phone: Faker::Company.duns_number, address: Faker::Address.street_name)
   Category.create(title: Faker::Commerce.department)
-  Product.create(title: Faker::Food.fruits, description: Faker::Food.description, price: Faker::Commerce.price)
+  Product.create(title: Faker::Food.fruits, description: Faker::Food.description, price: Faker::Commerce.price, image: 'https://images.unsplash.com/photo-1509223197845-458d87318791')
 end
 
 Market.all.each { |market| market.categories << Category.all }
